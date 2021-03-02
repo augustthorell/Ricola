6f07737408ff957b934e6fc714693cac83de42aa
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-02 16:35:22 +0100

    Zoom funcion, buttons showing/disappearing
    Zoom function is now functional and working on all three of the main buttons. The buttons toggle between showing and disappearing when clicked. A new back button is included for better navigation.

--- src/App.js
+++ src/App.js
@@ -1,73 +1,85 @@
 import './css/App.css'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Start from './components/start.js'
-import image from './assets/startBackground.jpg'
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
+import { motion } from 'framer-motion'
 
-/* function App() {
+function App() {
+	const [isActive, setIsActive] = useState(false)
+
+	const [xValueActive, setXValueActive] = useState()
+
+	const yValue = '-50%'
+
+	const variant = {
+		active: {
+			x: xValueActive,
+			y: yValue,
+			scale: 2,
+		},
+		inactive: {
+			x: 0,
+			y: 0,
+			scale: 1,
+		},
+	}
+	console.log(variant.active)
 	return (
-		<div className="App">
+		<div>
 			<Start />
+			<motion.div
+				className="startComponent"
+				variants={variant}
+				animate={isActive ? 'active' : 'inactive'}
+				transition={transition}
+			></motion.div>
+
+			<button
+				id="buttonNature"
+				className={isActive ? 'button-not-showing' : 'button-showing'}
+				onClick={() => {
+					setIsActive(!isActive)
+					setXValueActive('50%')
+				}}
+			>
+				Nature
+			</button>
+			<button
+				id="buttonBusiness"
+				className={isActive ? 'button-not-showing' : 'button-showing'}
+				onClick={() => {
+					setIsActive(!isActive)
+					setXValueActive('0%')
+				}}
+			>
+				Business
+			</button>
+			<button
+				id="buttonPeople"
+				className={isActive ? 'button-not-showing' : 'button-showing'}
+				onClick={() => {
+					setIsActive(!isActive)
+					setXValueActive('-50%')
+				}}
+			>
+				People
+			</button>
+			<button
+				id="buttonGoBack"
+				className={isActive ? 'button-showing' : 'button-not-showing'}
+				onClick={() => {
+					setIsActive(false)
+				}}
+			>
+				Back
+			</button>
 		</div>
 	)
 }
 
-export default App */
-
-export default class App extends Component {
-	state = {
-		type: true,
-		limitToBounds: true,
-		panningEnabled: true,
-		transformEnabled: true,
-		pinchEnabled: true,
-		limitToWrapper: false,
-		disabled: false,
-		dbClickEnabled: true,
-		lockAxisX: false,
-		lockAxisY: true,
-		velocityEqualToMove: true,
-		enableWheel: false,
-		enableTouchPadPinch: true,
-		enableVelocity: true,
-		limitsOnWheel: false,
-		centerContent: false,
-	}
-	render() {
-		const { lockAxisX, lockAxisY, enableWheel } = this.state
-		return (
-			<TransformWrapper
-				defaultScale={1}
-				defaultPositionX={100}
-				defaultPositionY={100}
-				pan={{
-					lockAxisX: true,
-					lockAxisY: true,
-				}}
-				wheel={{
-					wheelEnabled: enableWheel,
-				}}
-			>
-				{({
-					positionX = 10,
-					positionY = 5,
-					zoomIn = positionX,
-					zoomOut,
-					resetTransform,
-					...rest
-				}) => (
-					<React.Fragment>
-						<div className="tools">
-							<button onClick={zoomIn}>Nature</button>
-							<button onClick={resetTransform}>x</button>
-						</div>
-						<TransformComponent>
-							<img src={image} alt="test" style={{ width: '100vw' }} />
-							<div>Example text</div>
-						</TransformComponent>
-					</React.Fragment>
-				)}
-			</TransformWrapper>
-		)
-	}
+const transition = {
+	type: 'spring',
+	stiffness: 200,
+	damping: 100,
 }
+
+export default App
