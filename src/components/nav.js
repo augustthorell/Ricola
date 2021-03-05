a36b0c8b0968221840a59bc4a2d4df327a8378b4
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-05 11:45:27 +0100

    Added all the infoboxes with text to each button + styling.
    Added a container to each of the button infoboxes. Added content and the infoboxes to each induvidual button, also positioned the infoboxes. Styled the buttons, nature, people and business. Styled the navbar ar the top right corner.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import logo from '../assets/ricola-logo.png'
 
 function Nav() {
@@ -12,9 +12,21 @@ function Nav() {
 			</div>
 			<div className="navMenu">
 				<ul>
-					<Link to="/watch"><li>Watch</li></Link>
-					<Link to="/sustainabilityMagazine"><li>Sustainability Magazine</li></Link>
-					<Link to="/howWeWork"><li>How We Work</li></Link>
+					<Link to="/watch">
+						<li>
+							<span class="arrow">Watch</span>
+						</li>
+					</Link>
+					<Link to="/sustainabilityMagazine">
+						<li>
+							<span class="arrow">Sustainability Magazine</span>
+						</li>
+					</Link>
+					<Link to="/howWeWork">
+						<li>
+							<span class="arrow">How We Work</span>
+						</li>
+					</Link>
 				</ul>
 			</div>
 		</>
--- src/components/peopleInformation.js
+++ src/components/peopleInformation.js
@@ -2,18 +2,57 @@ import React from 'react'
 
 function PeopleInformation({ active }) {
 	return (
-		<div>
-			<div className="natureBoxInfo" id="natureBox1">
-				<h3>People Stuff</h3>
+		<div className="peopleContainer">
+			<div className="boxInfo" id="peopleBox1">
+				<h3>Continuous Deep Care</h3>
+				<p>
+					We want to show continuous deep care by growing and enriching the
+					lives of the people and communities we engage with, both as individual
+					human beings and as a business, for generations to come.
+				</p>
 			</div>
-			<div className="natureBoxInfo" id="natureBox2">
-				<h3>People Stuff</h3>
+			<div className="boxInfo" id="peopleBox2">
+				<h3>Communities</h3>
+				<p>
+					Establish principles to work with farmer communities globally by
+					standardising and expanding our pioneering eco-system approach to herb
+					farming in Switzerland.
+				</p>
 			</div>
-			<div className="natureBoxInfo" id="natureBox3">
-				<h3>People Stuff</h3>
+			<div className="boxInfo" id="peopleBox3">
+				<h3>Employees</h3>
+				<p>
+					Continue the implementation of our “Lean” leitbild Conduct regular
+					employee surveys by understanding and implementing state-of-the art
+					employee surveys. Increase awareness for sustainability by creating a
+					Ricola Sustainability Platform to deliver trainings and start
+					conversations around sustainability.
+				</p>
 			</div>
-			<div className="natureBoxInfo" id="natureBox4">
-				<h3>People Stuff</h3>
+			<div className="boxInfo" id="peopleBox4">
+				<h3>Supply Chain</h3>
+				<p>
+					Be responsive to social impacts in our global supply chain by
+					developing a monitoring and response system to continuously track,
+					understand and improve impacts in our herb sourcing and global supply
+					chain. Establish criteria for supplier selection to ensure fair
+					working conditions and high social standards in all services and
+					products in the value chain.
+				</p>
+			</div>
+			<div className="boxInfo" id="peopleBox5">
+				<h3>Partners</h3>
+				<p>
+					Establish criteria for contract manufacturers to ensure decent and
+					fair working conditions, non-discrimination and fair wages everywhere.
+				</p>
+			</div>
+			<div className="boxInfo" id="peopleBox6">
+				<h3>Consumers</h3>
+				<p>
+					Understand consumer expectations in relation to sustainability by
+					learning and understanding through consumer insights.
+				</p>
 			</div>
 		</div>
 	)
