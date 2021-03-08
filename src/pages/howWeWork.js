68c75505919efd868f6d3ad5dd1c1bc6f3a188b3
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-08 19:09:47 +0100

    Added boxes in "how we work" section
    Added content to how we work section (boxes, img and text).

--- src/pages/howWeWork.js
+++ src/pages/howWeWork.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import '../css/App.css'
 import Nav from '../components/nav.js'
+import image1 from '../assets/image-21.png'
+import image2 from '../assets/image-22.png'
 
 function HowWeWork() {
-    return (
-        <div>
-            <Nav />
-            <h2>How We Work</h2>
-        </div>
-    )
+	return (
+		<div className="howWeWorkContainer">
+			<Nav />
+			<div className="howWeWorkContent">
+				<h2>How We Work</h2>
+				<section className="articleContainer">
+					<div className="article">
+						<h4>Bee Health</h4>
+						<p>
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+							accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+							quae ab illo inventore veritatis et quasi architecto beatae vitae
+							dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+							aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+							eos qui ratione voluptatem sequi nesciunt.
+						</p>
+
+						<img src={image1} className="articleImg"></img>
+					</div>
+					<div className="article">
+						<h4>Swiss herbs</h4>
+						<p>
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+							accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+							quae ab illo inventore veritatis et quasi architecto beatae vitae
+							dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+							aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+							eos qui ratione voluptatem sequi nesciunt.
+						</p>
+
+						<img src={image2} className="articleImg"></img>
+					</div>
+				</section>
+			</div>
+		</div>
+	)
 }
 
 export default HowWeWork
