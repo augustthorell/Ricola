38029d3bd4cc0fcf7116f9b0c40a8c89de4cab64
fannyjohaaansson <fanny.johansson@hyperisland.se>
2021-03-11 09:47:43 +0100

    Added an article to how we work

--- src/pages/howWeWork.js
+++ src/pages/howWeWork.js
@@ -3,6 +3,7 @@ import '../css/App.css'
 import Nav from '../components/nav.js'
 import image1 from '../assets/image-21.png'
 import image2 from '../assets/image-22.png'
+import image3 from '../assets/image-19.png'
 
 function HowWeWork() {
 	return (
@@ -27,6 +28,22 @@ function HowWeWork() {
 					<div className="article">
 						<h4>Swiss herbs</h4>
 						<p>
+							Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit
+							aut fugit, sed quia consequuntur magni inventore veritatis
+							etdolores eos qui ratione voluptatem sequi nesciunt.
+							<br />
+							<br />
+							Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+							accusantium doloremque laudantium, magni dolores eos quitotam rem
+							aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+							architecto beatae vitae dicta sunt explicabo.
+						</p>
+
+						<img src={image2} className="articleImg"></img>
+					</div>
+					<div className="article">
+						<h4>Employee satisfaction</h4>
+						<p>
 							Sed ut perspiciatis unde omnis iste natus error sit voluptatem
 							accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
 							quae ab illo inventore veritatis et quasi architecto beatae vitae
@@ -35,7 +52,7 @@ function HowWeWork() {
 							eos qui ratione voluptatem sequi nesciunt.
 						</p>
 
-						<img src={image2} className="articleImg"></img>
+						<img src={image3} className="articleImg"></img>
 					</div>
 				</section>
 			</div>
